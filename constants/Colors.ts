f6d007a9c2d01e490a53ae750d5376bc04ac62cb
yiays <yiays@yiays.com>
2025-07-03 22:19:31 +1200

    Improve tint colours

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,5 +1,5 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#ddd';
+const tintColorLight = '#0275ac';
+const tintColorDark = '#9296ca';
 
 // Colour palette; https://coolors.co/palette/d72638-6091F3-a014af-140f2d-000000
 
@@ -17,7 +17,7 @@ export default {
     text: '#fff',
     background: '#000',
     tint: tintColorDark,
-    inactive: '#aaa',
+    inactive: '#888',
     danger: '#D72638',
     action: '#6091F3',
     theme: '#A014AF',
